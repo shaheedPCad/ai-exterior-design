0c883773ebaf0ebccfac410701bac10f6cb92a8d
Shaheed <shaheedPCad@gmail.com>
2024-01-14 18:44:58 -0500

    initial commit

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,6 @@
 import { LandingContent } from "@/components/landing-content";
 import { LandingHero } from "@/components/landing-hero";
 import { LandingNavbar } from "@/components/landing-navbar";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
